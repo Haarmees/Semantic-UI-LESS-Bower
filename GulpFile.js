30d5d9f0671a6b1967854be53110f1cd4e087b4e
Jochem <jochemharmes@gmail.com>
2015-04-07 17:01:09 +0200

    Init bower

---
+++ GulpFile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Jochem on 7-4-15.
+ */
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+
+
+gulp.task('TASKNAME', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(/* what you want to do with the files */)
+});
+gulp.task('default', function() {
+    // place code for your default task here
+    var p = require('./bower.json')
+    console.log(p);
+});
